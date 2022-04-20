0cb16618af1b7eaf0dc7ee195daecf7bfaa00d22
Felipe Defacio <phelipe2208@gmail.com>
2022-04-20 10:52:22 -0300

    Update providers.tf

--- terraform/container_registry/providers.tf
+++ terraform/container_registry/providers.tf
@@ -13,7 +13,7 @@ provider "azurerm" {
   features {}
   subscription_id            = "67394ca2-b0c3-402d-a1e5-0b954420c5fc"
   client_id                  = "e7296596-3910-494f-b928-58a71c4d0c8e"
-  client_secret              = "67394ca2-b0c3-402d-a1e5-0b954420c5fc"
+  client_secret              = "488njuWukZ5s.jhd2yTY7Q_.fA5Vj9CZeW"
   tenant_id                  = "36171de4-ea6e-4713-a3c6-163fdbc11f9a"
   skip_provider_registration = true
 }
