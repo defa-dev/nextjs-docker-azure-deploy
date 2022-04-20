b9fc15761e2dbe1d47d8b0ab838b7d9074d285d3
Felipe Defacio <phelipe2208@gmail.com>
2022-04-20 10:45:27 -0300

    feat: provider config

--- terraform/container_registry/providers.tf
+++ terraform/container_registry/providers.tf
@@ -11,10 +11,9 @@ terraform {
 
 provider "azurerm" {
   features {}
-#   subscription_id            = var.gcc_subscription_id
-#   client_id                  = var.gcc_client_id
-#   client_secret              = var.gcc_client_secret
-#   tenant_id                  = var.gcc_tenant_id
-#   enviroment                 = var.enviroment
-#   skip_provider_registration = true
+  subscription_id            = "67394ca2-b0c3-402d-a1e5-0b954420c5fc"
+  client_id                  = "e7296596-3910-494f-b928-58a71c4d0c8e"
+  client_secret              = "67394ca2-b0c3-402d-a1e5-0b954420c5fc"
+  tenant_id                  = "36171de4-ea6e-4713-a3c6-163fdbc11f9a"
+  skip_provider_registration = true
 }
