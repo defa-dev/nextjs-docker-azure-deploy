daa904bf70923abb950091b3408cad8e4d5f08e7
Felipe Defacio <phelipe2208@gmail.com>
2022-04-20 11:47:12 -0300

    feat: output

--- terraform/container_registry/main.tf
+++ terraform/container_registry/main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "terraform-rg-t01"
+  name     = "terraform-rg-t02"
   location = "eastus"
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "terraformcrt01"
+  name                = "terraformcrt02"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Premium"
